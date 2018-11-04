566a51f6c8580634108ebd7752ab7ff63d525bae
amagnan <arnauze@gmail.com>
2018-11-04 04:07:37 -0800

    Added sprinting option and more error handling

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: amagnan <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/01 16:00:24 by amagnan           #+#    #+#             */
-/*   Updated: 2018/11/03 01:11:03 by amagnan          ###   ########.fr       */
+/*   Updated: 2018/11/03 13:45:31 by feedme           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ void		start_modelization(t_variables *data)
 	data->mlx.mlx = mlx_init();
 	data->mlx.window = mlx_new_window(data->mlx.mlx, MAX_X, MAX_Y, "Wolf3D");
 	data->mlx.image = mlx_new_image(data->mlx.mlx, MAX_X, MAX_Y);
-	data->mlx.pixel = (unsigned char*)mlx_get_data_addr(data->mlx.image, &data->mlx.trash_a, &data->mlx.trash_b, &data->mlx.trash_c);
+	data->mlx.pixel = (unsigned char*)mlx_get_data_addr(data->mlx.image,
+		&data->mlx.trash_a, &data->mlx.trash_b, &data->mlx.trash_c);
 	algorithm(data);
 	mlx_hook(data->mlx.window, 2, 0, key_press, data);
 	mlx_hook(data->mlx.window, 3, 0, key_release, data);
@@ -26,7 +27,7 @@ void		start_modelization(t_variables *data)
 	mlx_loop(data->mlx.mlx);
 }
 
-void		wolf(t_variables *data)
+void		wolf3d(t_variables *data)
 {
 	init_player(data);
 	start_modelization(data);
@@ -41,8 +42,8 @@ int 		main(int argc, char **argv)
 	if (argc > 1)
 	{
 		fd = open(argv[1], O_RDONLY);
-		data->map = get_map(argv[1]);
-		wolf(data);
+		data->map = get_map(argv[1], NULL);
+		wolf3d(data);
 	}
 	return (0);
 }
